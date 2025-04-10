0cf34815113c4d33e2ad47f6c5222f3cc5a5e949
Fernando Balieiro Ferreira <fernando.balieiro@outlook.com>
2025-04-10 16:46:30 -0300

    change nvim config

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -10,40 +10,29 @@
 --     end
 -- }
 
-
-
-
-
 -- rose pine theme
-return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		require("rose-pine").setup({
-			variant = "moon"
-		})
-		vim.cmd("colorscheme rose-pine")
-	end
-}
-
-
-
-
-
--- tokyo night theme
 -- return {
--- 	"folke/tokyonight.nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	opts = {},
---
--- 	config = function ()
--- 		vim.cmd("colorscheme tokyonight-storm")
+-- 	"rose-pine/neovim",
+-- 	name = "rose-pine",
+-- 	config = function()
+-- 		require("rose-pine").setup({
+-- 			variant = "moon"
+-- 		})
+-- 		vim.cmd("colorscheme rose-pine")
 -- 	end
 -- }
 
+-- tokyo night theme
+return {
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
 
-
+	config = function ()
+		vim.cmd("colorscheme tokyonight-night")
+	end
+}
 
 -- dracula theme
 -- return { 
--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,13 +1,24 @@
+---@diagnostic disable: missing-fields
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "lua", "typescript", "go", "rust" },
-			highlight = { enabled = true },
-			ident= { enabled = true },
+			ensure_installed = {
+				"lua",
+				"typescript",
+				"go",
+				"rust",
+			},
+			highlight = {
+				enabled = true
+			},
+			ident= {
+				enabled = true
+			},
 		})
+
 
 	end
 }
